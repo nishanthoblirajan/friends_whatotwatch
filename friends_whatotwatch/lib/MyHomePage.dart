7256a74210809d596cb21100e38c905a53181f92
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-22 15:22:48 +0530

    initial commit

---
+++ friends_whatotwatch/lib/MyHomePage.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void _refresh(){
+
+    //TODO refresh
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _refresh,
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
